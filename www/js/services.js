03679e5d0bfa9eef8a1c5b4347d5c46626cf1838
Matthew Landauer <matthew@oaf.org.au>
2014-11-29 13:59:51 +1100

    Slot in a couple of campsites

--- www/js/services.js
+++ www/js/services.js
@@ -8,10 +8,40 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
+    {
+      id: 0,
+      shortName: "Acacia Flat",
+      longName: "Acacia Flat",
+      latitude: -33.6149,
+      longitude: 150.3553,
+      toilets: "non_flush",
+      picnicTables: false,
+      barbecues: "wood",
+      showers: "none",
+      drinkingWater: false,
+      caravans: false,
+      trailers: false,
+      car: false,
+      description: "Explore the \"cradle of conservation\", the Blue Gum Forest. Enjoy birdwatching, long walks and plenty of photogenic flora.",
+      park_id: 1
+    },
+    {
+      id: 1,
+      shortName: "Euroka (trailer area)",
+      longName: "Euroka campground - Appletree Flat campervan and camper trailer area",
+      latitude: -33.80002,
+      longitude: 150.61707,
+      toilets: "non_flush",
+      picnicTables: true,
+      barbecues: "wood",
+      showers: "none",
+      drinkingWater: false,
+      caravans: false,
+      trailers: true,
+      car: true,
+      description: "See the general Euroka campground information for facilities and activities. NOTE: Bring your own firewood - collecting firewood from the bush at Euroka is prohibited. \n\nPlease note that the campervan sites are suitable for small campervans or campertrailers - the sites are adjacent to the road edge.",
+      park_id: 1
+    }
   ];
 
   return {
